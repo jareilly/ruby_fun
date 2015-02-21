f0632f9b94f7b92a612a06c27bdabf0c8c5b61a7
James Reilly <reilly@iki.fi>
2015-02-21 21:04:48 +0200

    simple fibonacci
    needs improving

--- 03_fibonacci/lib/fibonacci.rb
+++ 03_fibonacci/lib/fibonacci.rb
@@ -1,3 +1,10 @@
 def fibonacci(n)
-  -1
+  if n < 2
+    n
+  else
+    # recursively Fn = Fn-1 + Fn-2
+    if (n > 0)
+      fibonacci(n-1) + fibonacci(n-2)
+    end
+  end
 end
--- 03_fibonacci/minitests/fibonacci_test.rb
+++ 03_fibonacci/minitests/fibonacci_test.rb
@@ -3,9 +3,13 @@ require 'minitest/spec'
 
 require 'fibonacci'
 
+# 1 1 2 3 5 ...
 describe "test" do
   it "test" do
-  	f = fibonacci(2)
-    assert f == 3
+  	assert fibonacci(1) == 1, "1"
+    assert fibonacci(2) == 1, "2: "
+    assert fibonacci(14) == 377 , "14"
+    assert fibonacci(25) == 75025 , "14"
+    #assert fibonacci(100) == 354224848179261915075, "100"
   end
 end
