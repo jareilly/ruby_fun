34298e0a3304dedb5a0943782322647303052f1a
James Reilly <reilly@iki.fi>
2015-02-21 21:33:37 +0200

    better implementation

--- 03_fibonacci/lib/fibonacci.rb
+++ 03_fibonacci/lib/fibonacci.rb
@@ -1,3 +1,12 @@
+#memoization to record earlier calculations, from a stackexchange post
+def fib(n, cache = {})
+  if n == 0 || n == 1
+    return n
+  end
+  cache[n] ||= fib(n-1, cache) + fib(n-2, cache)
+end
+
+# naive slow implementation
 def fibonacci(n)
   if n < 2
     n
--- 03_fibonacci/minitests/fibonacci_test.rb
+++ 03_fibonacci/minitests/fibonacci_test.rb
@@ -6,10 +6,12 @@ require 'fibonacci'
 # 1 1 2 3 5 ...
 describe "test" do
   it "test" do
-  	assert fibonacci(1) == 1, "1"
-    assert fibonacci(2) == 1, "2: "
-    assert fibonacci(14) == 377 , "14"
-    assert fibonacci(25) == 75025 , "14"
-    #assert fibonacci(100) == 354224848179261915075, "100"
+  	#assert fibonacci(1) == 1, "1"
+    #assert fibonacci(2) == 1, "2: "
+    #assert fibonacci(14) == 377 , "14"
+    #assert fibonacci(25) == 75025 , "25"
+    #assert fibonacci(40) == 102334155 , "40"
+
+    assert fib(100) == 354224848179261915075, "100"
   end
 end
