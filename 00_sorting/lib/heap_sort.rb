e6217207aa543afd6b34e017c25d7394073631c0
James Reilly <reilly@iki.fi>
2015-02-28 09:44:49 +0200

    added heap sort

--- 00_sorting/lib/heap_sort.rb
+++ 00_sorting/lib/heap_sort.rb
@@ -1,8 +1,37 @@
 # Heapsort is O(n log n)
+# Following wikipedia psuedo-code
 
-def heap_sort(array)
-  return [] if array.empty? || array.nil?
+def heap_sort!(array)
+  build_max_heap(array)
 
-  # Fail
-  return []
+  last_item_index = array.length - 1
+  (last_item_index).downto(1) do |i|
+    array[0], array[i] = array[i], array[0]
+    heapify(array, i, 0)
+  end
+  array
+end
+
+def build_max_heap(array)
+  ((array.length/2) - 1).downto(0) do |i|
+    heapify(array, array.length, i)
+  end
+  array
+end
+
+def heapify(array, length, i)
+  left_index  = (2*i) + 1
+  right_index = (2*i) + 2
+  if left_index < length and array[left_index] > array[i]
+    largest_index = left_index
+  else
+    largest_index = i
+  end
+  if right_index < length and array[right_index] > array[largest_index]
+    largest_index = right_index
+  end
+  if (largest_index != i)
+    array[i], array[largest_index] = array[largest_index], array[i]
+    heapify(array, length, largest_index)
+  end
 end
--- 00_sorting/minitests/heap_sort_test.rb
+++ 00_sorting/minitests/heap_sort_test.rb
@@ -7,19 +7,19 @@ describe "test" do
   it "sorts a simple array" do
     sorted = [1, 2, 3, 4, 5]
     unsorted = sorted.shuffle
-    assert heap_sort(unsorted).must_equal sorted
+    assert heap_sort!(unsorted).must_equal sorted
   end
 
   it "sorts arrays with duplicate elements" do
     sorted = [1, 2, 2, 3, 3, 3, 4, 5]
     unsorted = sorted.shuffle
-    assert heap_sort(unsorted).must_equal sorted
+    assert heap_sort!(unsorted).must_equal sorted
   end
 
   it "should sort string arrays too" do
     sorted = %w{ apple banana grapes kiwi pear}
     unsorted = sorted.shuffle
-    assert heap_sort(unsorted).must_equal sorted
+    assert heap_sort!(unsorted).must_equal sorted
   end
 
   #it "should sort arbirtarily large arrays" do
